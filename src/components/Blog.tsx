f11079ca35e9380297d7414d6594b11cfce080ed
Annas <annaskherunas06@gmail.com>
2024-10-20 22:06:30 +0700

    fix a bunch of stuff and make the website better

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -24,46 +24,49 @@ export default function Blog(props: Props) {
     }, [props.markdownContentPath]);
 
     return (
-        <div className="blog">
-            <div className="blog-header">
-                <SvgButton onClick={() => navigate("/blogs")}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="50" height="50">
-                        {/* Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc */}
-                        <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/>
-                    </svg>
-                </SvgButton>
+        <>
+            <div className="blog">
+                <div className="blog-header">
+                    <SvgButton onClick={() => navigate("/blogs")}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="50" height="50">
+                            {/* Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc */}
+                            <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+                        </svg>
+                    </SvgButton>
 
-                <p className="blog-title">{props.title}</p>
-                
-                <p className="blog-date">{props.date}</p>
+                    <p className="blog-title">{props.title}</p>
+
+                    <p className="blog-date">{props.date}</p>
+                </div>
+
+                <Markdown
+                    className="markdown-content"
+                    components={{
+                        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                        code({ node, className, children, ...props }) {
+                            const match = /language-(\w+)/.exec(className || "");
+                            return match ? (
+                                <SyntaxHighlighter
+                                    style={oneDark}
+                                    language={match[1]}
+                                    PreTag="div"
+                                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                                    {...props as any}
+                                >
+                                    {String(children).replace(/\n$/, "")}
+                                </SyntaxHighlighter>
+                            ) : (
+                                <code className={className} {...props}>
+                                    {children}
+                                </code>
+                            );
+                        },
+                    }}
+                >
+                    {markdownContent}
+                </Markdown>
             </div>
-
-            <Markdown
-                className="markdown-content"
-                components={{
-                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                    code({ node, className, children, ...props }) {
-                        const match = /language-(\w+)/.exec(className || "");
-                        return match ? (
-                            <SyntaxHighlighter
-                                style={oneDark}
-                                language={match[1]}
-                                PreTag="div"
-                                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                                {...props as any}
-                            >
-                                {String(children).replace(/\n$/, "")}
-                            </SyntaxHighlighter>
-                        ) : (
-                            <code className={className} {...props}>
-                                {children}
-                            </code>
-                        );
-                    },
-                }}
-            >
-                {markdownContent}
-            </Markdown>
-        </div>
+            <div className="spacing" />
+        </>
     );
 }
