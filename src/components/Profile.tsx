793b24a0667977223f9ab8b575624ffa2882d030
Annas <annaskherunas06@gmail.com>
2025-03-31 08:48:56 +0700

    change pfp

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,24 +1,12 @@
-import { useState } from "react";
 import "./Profile.css";
 
-import annasNormalPath from "../assets/annas/annas_normal.png";
-import annasHappyPath from "../assets/annas/annas_happy.png";
+import cat from "../assets/cat/cat.jpg";
 
 export default function Profile() {
-    const [isMouseDown, setIsMouseDown] = useState(false);
-
-    function onMouseEnter() {
-        setIsMouseDown(true);
-    }
-
-    function onMouseLeave() {
-        setIsMouseDown(false);
-    }
-
     return (
         <div className="profile">
             <div className="profile-img-container">
-                <img className="profile-img" src={isMouseDown ? annasHappyPath : annasNormalPath} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} />
+                <img className="profile-img" src={cat} />
             </div>
 
             <p className="profile-description">Hi, my name is Annas i like to code and draw</p>
