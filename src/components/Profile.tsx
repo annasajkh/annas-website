c6499fd6306c1311cea68fadd5d1ab0269e08707
Annas <annaskherunas04@gmail.com>
2026-01-18 08:53:09 +0700

    change quote

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -10,7 +10,7 @@ export default function Profile() {
             </div>
 
             <p className="profile-description">Hi, my name is Annas i like to code and draw</p>
-            <p className="quote">"The entire universe is the emergent properties of rules, we tried to discover and make sense of the rules using multiple level of abstractions, we use math to map the rules to things we can calculate, but not all rules can be mapped to math, but it doesn't stop us for making sense of the rules, down from the quantum level using quantum mechanics up to human intelligence and human behaviors using psychology, social systems, and neuroscience, and galaxies using general relativity, special relativity, thermodynamics, and entropy, i said this before i was going to celebrate Isra' and Mi'raj on my religion that i was born into that is islam on my local mosque"</p>
+            <p className="quote">"The entire universe is the emergent properties of rules, we tried to discover and make sense of the rules using multiple level of abstractions, we use math to map the rules to things we can calculate, many of the rules are too complex if you try to map it math, you can do that maybe if you try hard enough, but we often use our logical assumptions to makes sense of these rules, example human behaviors, but it doesn't stop us for making sense of the rules, down from the quantum level using quantum mechanics up to human intelligence and human behaviors using psychology, social systems, and neuroscience, and galaxies using general relativity, special relativity, thermodynamics, and entropy, i said this before i was going to celebrate Isra' and Mi'raj on my religion that i was born into that is islam on my local mosque"</p>
         </div>
     );
 }
